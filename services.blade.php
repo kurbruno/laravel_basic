ed2cf980b86c65410c7f61d940a75e0ba0202990
kurbruno <bruno.babincevs@gmail.com>
2025-01-08 17:47:02 +0200

    Add files via upload

---
+++ services.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Services</title>
+</head>
+
+<body>
+    <div>
+        <h1>Our Services :</h1>
+        <ul>
+            <li>Fiber-optic internet</li>
+            <li>Mobile Internet</li>
+            <li>Data network</li>
+            <li>Network Shield</li>
+            <li>Phishing attack simulation training</li>
+            <li>Network Protection Solutions</li>
+        </ul>
+    </div>
+
+
+</body>
+
+</html>
